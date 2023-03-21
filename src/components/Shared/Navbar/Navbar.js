e022a2a87fd4ce6ee89e5988404efbd2fc311f2e
“atikdev-bd” <atikdev.bd@gmail.com>
2023-03-21 17:53:18 +0600

    navbar added

---
+++ src/components/Shared/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import simIcon from "../../../Assets/titleIcon/icons8-sim-card-64.png";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-300">
+      <div className="flex-1">
+        <img src={simIcon} alt="" />
+        <a className="btn btn-ghost normal-case text-xl">Offer.com</a>
+
+        <div className="flex gap-5 ml-10">
+          <div>
+            <Link to='/home'>Home</Link>
+          </div>
+          <div>
+            <Link to='login'>Login</Link>
+          </div>
+          <div>
+            <Link to='register'>Register</Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex-none">
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+          ></div>
+        </div>
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
